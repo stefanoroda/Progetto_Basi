8a95916d787524049a0bae679c2103c320a1067d
Tobia Sacchetto <tobia.sacchetto@gmail.com>
2023-06-02 11:45:49 +0200

    Make Libro easier

--- Web-interface/php/Libro.php
+++ Web-interface/php/Libro.php
@@ -23,52 +23,53 @@
             <fieldset>
                 <label>Inserire titolo:</label>
                 <input type="text" name="TITOL">
+                <input type="submit" value="invio"> 
             </fieldset>
-            <input type="submit" value="invio"> 
         </form>
-        
+        <a href="../index.html">Home</a>
+        <br><br>
+        <table>        
+        <tr>
+            <th>Titolo</th>
+            <th>ISBN</th>
+            <th>Succursale</th>
+        </tr>
+        <?php
+
+            $TITOL   = $_POST['TITOL'];
+
+            try{
+                $sql = "SELECT * FROM LIBRO WHERE TITOLO LIKE '%$TITOL%'";
+                
+
+
+                $query = mysqli_query($link, $sql);
+                
+
+            }catch(mysqli_sql_exception $e){
+                echo "Error: " . $e->getMessage();
+            }
+
+            $libro=array();
+            $i=0;
+            while($row= mysqli_fetch_array($query)){
+                $libro[$i]= $row;
+                $i++;
+            }
+
+            foreach($libro as $lib):
+                echo"<tr>";
+                echo "<td>$lib[1]</td>";
+                echo "<td>$lib[2]</td>";
+                echo "<td>$lib[3]</td>";
+                echo "</tr>";
+            endforeach;
+            mysqli_close($link);
+
+
+        ?>
+        </table>
     </body>
 </html>
 
 
-<?php
-
-$TITOL   = $_POST['TITOL'];
-
-try{
-    $sql = "SELECT * FROM LIBRO WHERE TITOLO LIKE '%$TITOL%'";
-    
-
-
-    $query = mysqli_query($link, $sql);
-    
-
-}catch(mysqli_sql_exception $e){
-    echo "Error: " . $e->getMessage();
-}
-
-$libro=array();
-$i=0;
-while($row= mysqli_fetch_array($query)){
-    $libro[$i]= $row;
-    $i++;
-}
-
-foreach($libro as $lib):
-    echo $lib[0];
-    echo $lib[1];
-    echo $lib[2];
-    echo $lib[3];
-endforeach;
-mysqli_close($link);
-
-
-
-
-    
-
-
-
-
-
-?>
