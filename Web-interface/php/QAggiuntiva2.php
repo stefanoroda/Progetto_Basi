03ec3c64a59a5a1fc7b901e4ecd6c687c5b71121
Tobia Sacchetto <tobia.sacchetto@gmail.com>
2023-06-09 19:00:33 +0200

    FATTO IL MISFATTO

---
+++ Web-interface/php/QAggiuntiva2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>QueryAg2</title>
+        <link rel="stylesheet" href="css/retro.css">
+        <link rel="stylesheet" href="css/mycss.css">
+    </head>
+    <body>
+        <h1>Query aggiuntiva 2</h1>
+        <p></p>
+    </body>
+</html>
