e7887301be99966797c31f74844a62eee652edf4
Tobia Sacchetto <tobia.sacchetto@gmail.com>
2023-06-09 14:02:38 +0200

    adding query

---
+++ Web-interface/php/RPrestito.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Autori</title>
+        <link rel="stylesheet" href="../css/retro.css">
+        <link rel="stylesheet" href="../css/mycss.css">
+    </head>
+    <body>
+        <h1>Ricerca Prestito</h1>
+        <p></p>
+        <?php
+        echo "Today is " . date("Y-m-d") . "<br>";
+        $DATA_OGGI=strtotime(date("Y-m-d"));
+        $DATA_SCADENZA=strtotime("+1 month", $DATA_OGGI);
+        echo date("Y-m-d", $DATA_SCADENZA);
+        ?>
+    </body>
+</html>
